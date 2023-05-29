092535420ec8913cb4a70e205f299901327b6952
Younger Shen <shenyangang@163.com>
2023-05-29 17:14:41 +0800

    FEATURE: 添加快排算法

---
+++ Doc/算法/排序/quick_sort.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+
+void print_int_array(int * a, int l)
+{
+    for(int i = 0; i < l; i ++)
+    {
+        printf("%d", *(a+i));
+
+        if( i != l - 1)
+        {
+            printf(", ");
+        }
+        else
+        {
+            printf("\r\n");
+        }
+    }
+}
+
+int get_randint(int min, int max)
+{
+    int i;
+    i = rand() % max + min;  // 生成 10 位随机整数
+    return i;
+}
+
+void get_randint_array(int* a, int l)
+{
+    for(int i=0; i < l; i ++)
+    {
+        int n = get_randint(MIN, MAX);
+        *(a+i) = n;
+    }
+}
+
+void quick_sort(int* a, int l)
+{
+
+}
+
+int main(int* argv, int argc)
+{
+
+}
+
