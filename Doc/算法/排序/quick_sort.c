0f86374242261364ef567d454618033c70e23954
Younger Shen <shenyangang@163.com>
2023-05-30 09:52:15 +0800

    FEATURE: 新增快速排序算法

--- Doc/算法/排序/quick_sort.c
+++ Doc/算法/排序/quick_sort.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include<stdbool.h>
 #define MIN 10
 #define MAX 100
 
@@ -37,11 +38,45 @@ void get_randint_array(int* a, int l)
     }
 }
 
-void take_partation(int * a, int low, int high)
+//  两个下标交换的 partation
+int partation2(int * a, int low, int high)
+{
+    int left = low;
+    int right = high;
+    int pivot = *(a + high);
+
+
+    while(low < high)
+    {
+        while(*(a + left) < pivot)
+            left+=1;
+
+        while(*(a + right) > pivot)
+            right -= 1;
+
+
+        if(left >= right)
+        {
+            printf("found\r\n");
+            return right;
+        }
+
+        printf("swap\r\n");
+        int temp = *(a + left);
+        *(a + left) = *(a + right);
+        *(a + right) = temp;
+    }
+
+}
+
+// 一个下标交换的 partation
+int partation1(int * a, int low, int high)
 {
     // pivot 的位置
     int index = low - 1;
     // 取数组的末尾值为 pivot
+    // 这个时候 high 即为 pivot 的下标
+    // 最后交换的时候 使用 high 与实际的位置的值进行交换
     int pivot = *(a + high);
 
     // 在循环中进行比较和替换
@@ -51,14 +86,27 @@ void take_partation(int * a, int low, int high)
         {
             // 只要当前的值小于 pivot, 那么位置就自增一
             index += 1;
+            int temp = *(a + index);
+            *(a + index) = * (a + i);
+            *(a + i) = temp;
         }
     }
 
+    int temp = * ( a + index + 1);
+    *(a + index + 1) = *(a + high);
+    *(a + high) = temp;
+
+    return index + 1;
 }
 
-void quick_sort(int* a, int l)
+void quick_sort(int* a, int low, int high)
 {
-
+    if(low >= 0 && high >= 0 && high > low)
+    {
+        int pivot = partation1(a, low, high);
+        quick_sort(a, low, pivot - 1);
+        quick_sort(a, pivot + 1, high);
+    }
 }
 
 int main(int* argv, int argc)
@@ -67,7 +115,10 @@ int main(int* argv, int argc)
     srand(time(NULL));
     int len = 10;
     int arr[len];
+    int low = 0;
+    int high = len - 1;
     get_randint_array(arr, len);
+    printf("Quick Sort demonstration \r\n");
 
     printf("\r\n");
     printf("Before Sort: \r\n");
@@ -75,6 +126,9 @@ int main(int* argv, int argc)
     printf("\r\n");
     print_int_array(arr, len);
     printf("\r\n");
+
+    // quick sort
+    quick_sort(arr, low, high);
 
     printf("After Sort: \r\n");
     printf("\r\n");
